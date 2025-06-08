46577bbe30c7ee9777cc09aad57b397b169e4163
gruber <nikas.g.ng@gmail.com>
2025-06-08 13:34:05 +0200

    now calls coin counter

--- level_addons/scripts/coin.gd
+++ level_addons/scripts/coin.gd
@@ -15,6 +15,7 @@ func _on_body_entered(body: Node2D) -> void:
 		#collission.disabled = true
 		shadow.texture = null
 		audio.play()
+		get_tree().call_group("coin_counter", "count")
 
 func _on_audio_stream_player_2d_finished() -> void:
 	call_deferred("queue_free")
